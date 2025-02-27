e5bcd9164795056b8f6dc4207158663d4298c4e9
Somashekhar Bidari <somashekharvb@gmail.com>
2025-02-27 17:55:37 +0100

    healthcheck-json dict

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "server side error and could not process request", http.StatusInternalServerError)
+		return
+	}
+
 }
