64afc1e917ffeb12a36e5fbcf93a0cfa28db8497
Somashekhar Bidari <somashekharvb@gmail.com>
2025-03-11 22:20:21 +0100

    handling partial updates

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -95,9 +95,9 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	var input struct {
-		Title   string       `json:"title"`
-		Year    int          `json:"year"`
-		Runtime data.Runtime `json:"runtime"`
+		Title   *string       `json:"title"`
+		Year    *int          `json:"year"`
+		Runtime *data.Runtime `json:"runtime"`
 		Genres  []string     `json:"genres"`
 	}
 
@@ -107,11 +107,19 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	movie.Title = input.Title
-	movie.Year = input.Year
-	movie.Runtime = input.Runtime
-	movie.Genres = input.Genres
-
+	if input.Title != nil{
+		movie.Title = *input.Title
+	}
+	if input.Year != nil{
+		movie.Year = *input.Year
+	}
+	if input.Runtime != nil{
+		movie.Runtime = *input.Runtime
+	}
+	if input.Genres != nil{
+		movie.Genres = input.Genres
+	}
+	
 	v := validator.New()
 	if data.ValidateMovie(v, movie); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
